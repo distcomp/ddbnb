dc40d6a80dd25d8a16c841a7b6c691bf6bab51f4
Sergey Smirnov <sasmir@gmail.com>
2013-10-25 18:11:19 +0400

    Добавил сохранение логов мастером

--- src/dcbc_master.erl
+++ src/dcbc_master.erl
@@ -7,14 +7,15 @@
 
 -record(state, {best_val = none, best_sol = {none, none}, stubs, start_ts, solver_args}).
 
--record(subp, {pid = none, status = none, slave_pid = none, ref = none}).
+-record(subp, {path, pid = none, status = none, slave_pid = none, ref = none}).
 
 start_link(SolverArgs, FileNames) ->
     gen_server:start_link(?MODULE, [SolverArgs, FileNames], []).
 
 init([SolverArgs, FileNames]) ->
     gen_server:cast(self(), do_init),
-    Stubs = dict:from_list(lists:map(fun (Name) -> {Name, #subp{}} end, FileNames)),
+    {Pairs, _} = lists:mapfoldl(fun (Path, Id) -> {{Id, #subp{path = Path}}, Id + 1} end, 1, FileNames),
+    Stubs = dict:from_list(Pairs),
     {ok, #state{stubs = Stubs, solver_args = SolverArgs, start_ts = now()}}.
 
 handle_cast(do_init, State) ->
@@ -22,8 +23,9 @@ handle_cast(do_init, State) ->
     {noreply, initial_submit({State, Slaves})};
 handle_cast({best_val, Name, Val}, State) ->
     {noreply, update_best(Name, Val, none, State)};
-handle_cast({solver_done, Name, Status, Val, Sol, _Log}, State0) ->
-    io:format("~.3f Subproblem ~s status: ~p, result=~p~n", [seconds_elapsed(State0#state.start_ts), Name, Status, Val]),
+handle_cast({solver_done, Name, Status, Val, Sol, Log}, State0) ->
+    io:format("~.3f Subproblem ~p status: ~p, result=~p~n", [seconds_elapsed(State0#state.start_ts), Name, Status, Val]),
+    ok = file:write_file(integer_to_list(Name) ++ ".log", Log),
     State = update_best(Name, Val, Sol, State0),
     Subp = dict:fetch(Name, State#state.stubs),
     demonitor(Subp#subp.ref, [flush]),
@@ -50,15 +52,15 @@ find_next_problem(Stubs) ->
 submit_problem(Name, {State, []}) ->
     {State, []};
 submit_problem(Name, {State, [SlavePid | Tail] = Slaves}) ->
-    {ok, Data} = file:read_file(Name),
+    Subp = dict:fetch(Name, State#state.stubs),
+    {ok, Data} = file:read_file(Subp#subp.path),
     case dcbc_slave:start_solver(SlavePid, Name,
                                  [{best_val, State#state.best_val}, {stub, Data},
                                   {solver_args, State#state.solver_args}]) of
         {ok, SolverPid} ->
             Ref = monitor(process, SolverPid),
-            Subp = (dict:fetch(Name, State#state.stubs))#subp{
-                     pid = SolverPid, slave_pid = SlavePid, ref = Ref, status = running},
-            {State#state{stubs = dict:store(Name, Subp, State#state.stubs)}, Slaves};
+            Subp1 = Subp#subp{pid = SolverPid, slave_pid = SlavePid, ref = Ref, status = running},
+            {State#state{stubs = dict:store(Name, Subp1, State#state.stubs)}, Slaves};
         {error, _} ->
             submit_problem(Name, {State, Tail})
     end.
@@ -66,15 +68,15 @@ submit_problem(Name, {State, [SlavePid | Tail] = Slaves}) ->
 update_best(Name, Val, Sol, #state{best_val = BV1, best_sol = {BV2, _BS}} = State) ->
     case {is_better_than(Val, BV1, minimize), is_better_than(Val, BV2, minimize), Sol} of %% NOTE: minimization
         {true, true, Sol} when Sol =/= none ->
-            io:format("~.3f ~s New best_val and best_sol: ~p~n", [seconds_elapsed(State#state.start_ts), Name, Val]),
+            io:format("~.3f ~p New best_val and best_sol: ~p~n", [seconds_elapsed(State#state.start_ts), Name, Val]),
             broadcast_best_val(Name, Val, State#state.stubs),
             State#state{best_val = Val, best_sol = {Val, Sol}};
         {true, _, Sol} ->
-            io:format("~.3f ~s New best_val: ~p~n", [seconds_elapsed(State#state.start_ts), Name, Val]),
+            io:format("~.3f ~p New best_val: ~p~n", [seconds_elapsed(State#state.start_ts), Name, Val]),
             broadcast_best_val(Name, Val, State#state.stubs),
             State#state{best_val = Val};
         {false, true, Sol} when Sol =/= none ->
-            io:format("~.3f ~s New best_sol: ~p~n", [seconds_elapsed(State#state.start_ts), Name, Val]),
+            io:format("~.3f ~p New best_sol: ~p~n", [seconds_elapsed(State#state.start_ts), Name, Val]),
             State#state{best_sol = {Val, Sol}};
         _ ->
             State
@@ -111,7 +113,7 @@ handle_info({'DOWN', Ref, process, _Pid, Reason}, State) ->
                      fun (Name, S = #subp{ref = R}, none) when R =:= Ref -> {Name, S};
                          (_, _, Acc) -> Acc end,
                      none, State#state.stubs),
-    io:format("Solver crashed for subproblem ~s: ~p~n", [Name, Reason]),
+    io:format("Solver crashed for subproblem ~p: ~p~n", [Name, Reason]),
     Subp1 = Subp#subp{pid = none, ref = none, slave_pid = none, status = crashed},
     State1 = State#state{stubs = dict:store(Name, Subp1, State#state.stubs)},
     {noreply, submit_next_problem(State1, Subp#subp.slave_pid)}.
